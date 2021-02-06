5d6780ea86187406eb52f28453d167a634f2d49e
RonMiro <mirochnik.ron@gmail.com>
2021-02-06 19:06:11 +0200

    Probelm 1 part 1

--- Development/main.py
+++ Development/main.py
@@ -1,5 +1,4 @@
 from problem1 import problem1
 
-
 if __name__ == "__main__":
     problem1()
--- Development/problem1.py
+++ Development/problem1.py
@@ -1,5 +1,18 @@
+from typing import List
 
+
+def sumTwo(values: List[int], target: int) -> tuple[int, int]:
+    seen_values = set()
+
+    for val in values:
+        if target - val in seen_values:
+            return val, target-val
+        seen_values.add(val)
 
 
 def problem1():
-    print("Empty")
+    with open(r"C:\Users\User\Desktop\mega\edu\Projects\AoC\AdventOfCode\Development\assets\p1_input.txt", 'r')as file_input:
+        file_data = file_input.read()
+        values = [int(val) for val in file_data.strip().split("\n")]
+        val1, val2 = sumTwo(values, 2020)
+        print(val1*val2)
