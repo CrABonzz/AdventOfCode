ac5f159ebeb1801c05c7d764a1517e1209a40e62
RonMiro <mirochnik.ron@gmail.com>
2021-02-06 19:19:44 +0200

    Problem1 solved

--- Development/problem1.py
+++ Development/problem1.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import List
 
 
@@ -6,13 +7,31 @@ def sumTwo(values: List[int], target: int) -> tuple[int, int]:
 
     for val in values:
         if target - val in seen_values:
-            return val, target-val
+            return val, target - val
         seen_values.add(val)
+
+    raise ValueError("Not found")
+
+
+def sumThree(values: List[int], target: int) -> tuple[int, int, int]:
+    for val in values:
+        values.remove(val)
+
+        try:
+            val1, val2 = sumTwo(values, target - val)
+            return val, val1, val2
+        except ValueError:
+            values.append(val)
 
 
 def problem1():
-    with open(r"C:\Users\User\Desktop\mega\edu\Projects\AoC\AdventOfCode\Development\assets\p1_input.txt", 'r')as file_input:
+    with open(r".\assets\p1_input.txt",
+              'r')as file_input:
         file_data = file_input.read()
         values = [int(val) for val in file_data.strip().split("\n")]
-        val1, val2 = sumTwo(values, 2020)
-        print(val1*val2)
+
+        part1 = reduce(lambda x, y: (x * y), sumTwo(values, 2020))
+
+        part2 = reduce(lambda x, y: (x * y), sumThree(values, 2020))
+
+        print(part1, part2)
