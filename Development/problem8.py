331a1b84ccf65d688876c45e28c7faa5438e9efd
RonMiro <mirochnik.ron@gmail.com>
2021-02-20 19:47:19 +0200

    Problem 8 solved

--- Development/problem8.py
+++ Development/problem8.py
@@ -1,4 +1,8 @@
-def total_acc_one_run(raw_instructions):
+ACCUMALTOR_COMMAND = "acc"
+JMP_COMMAND = "jmp"
+NOP_COMMAND = "nop"
+
+def run_until_loop(raw_instructions):
     instructions = [(instruction.split(" ")[0], int(instruction.split(" ")[1])) for instruction in raw_instructions]
 
     accumulator = 0
@@ -6,25 +10,56 @@ def total_acc_one_run(raw_instructions):
     exectued_instructions = set()
 
     while True:
-        if current_instruction in exectued_instructions:
+        if current_instruction in exectued_instructions or \
+                current_instruction < 0 or current_instruction >= len(instructions):
             break
 
         exectued_instructions.add(current_instruction)
 
         command, argument = instructions[current_instruction]
-        if "acc" == command:
+
+        if ACCUMALTOR_COMMAND == command:
             accumulator += argument
             current_instruction += 1
-        elif "jmp" == command:
+        elif JMP_COMMAND == command:
             current_instruction += argument
-        else: # nop
+        else:  # NOP_COMMAND
             current_instruction += 1
 
-    print("Part 1: Accumulator end of first run", accumulator)
+    return accumulator, current_instruction
+
+
+def fix_program(instructions):
+    """
+    We need to make the program end, means the last instruction will be len(instructions)
+    """
+    for index, instruction in enumerate(instructions):
+        command = instruction.split(" ")[0]
+
+        if command == JMP_COMMAND:
+            new_command = NOP_COMMAND
+        elif command == NOP_COMMAND:
+            new_command = JMP_COMMAND
+        else:
+            continue
+
+        old_instruction = instructions[index]
+        instructions[index] = instruction.replace(command, new_command)
+
+        accumulator, last_instructions = run_until_loop(instructions)
+        if len(instructions) == last_instructions:
+            return accumulator
+
+        # Restore
+        instructions[index] = old_instruction
 
 
 def problem8():
     with open(r"assets\p8_input.txt", 'r') as file_input:
         intstructions = file_input.readlines()
 
-        total_acc_one_run(intstructions)
+        accumulator, _ = run_until_loop(intstructions)
+        print("Part 1: Accumulator end of first run", accumulator)
+
+        accumulator = fix_program(intstructions)
+        print("Part 2 Accumulator of the fixed program", accumulator)
