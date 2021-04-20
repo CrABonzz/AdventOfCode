e97e543d5023cdaf7e4d0e7651d0a0fb1ded5fa3
RonMiro <mirochnik.ron@gmail.com>
2021-04-20 22:20:27 +0300

    Update problem10.py

--- Development/problem10.py
+++ Development/problem10.py
@@ -1,15 +1,22 @@
-def part1(numbers):
-    numbers.sort()
-    numbers.insert(0, 0)    # The initial voltage
+def part2(numbers):
+    ways = {numbers[-1]: 1}     # The final step
 
-    differencess = [y - x for x, y in zip(numbers, numbers[1:])]
+    for number in reversed(numbers[:-1]):
+        ways[number] = sum(ways[number + next_diff] for next_diff in range(1, 4) if number + next_diff in numbers)
 
-    # The 3 counts plus 1 for the main adapter
-    return (differencess.count(3) + 1)*differencess.count(1)
+    return ways[0]
 
 
 def problem10():
     with open(r"assets\p10_input.txt", 'r')as file_input:
         numbers = [int(num) for num in file_input.readlines()]
 
-    print("Part 1: ", part1(numbers))
+    # Add initial and final voltages
+    numbers.sort()
+    numbers.insert(0, 0)
+    numbers.append(max(numbers) + 3)
+
+    differences = [y - x for x, y in zip(numbers, numbers[1:])]
+
+    part1 = "1-jolt multiplied by 3-jolt differences:" + str(differences.count(3) * differences.count(1))
+    print("The number of ways: ", part2(numbers))
